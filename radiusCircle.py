fc65a03c9754548fa1a98937f0a7fd967fbf400d
HP <mandeepkaur10890@gmail.com>
2021-05-19 16:40:58 +0530

    Initial commit

---
+++ radiusCircle.py
@@ -0,0 +1,8 @@
+
+def circle_radius(radius):
+   area=(22/7)*radius**2
+   print(f"Radius of circle is {radius} and area of circle is {str(area)}")
+def main():
+    radius=float(input("Enter radius of circle"))
+    circle_radius(radius)
+main()
